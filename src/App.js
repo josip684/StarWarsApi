3475d762a9df02e8f07409b36042cfd3786270a7
josip684 <goreta684@gmail.com>
2020-01-13 19:16:35 +0100

    Decorate front page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.png';
+import dartWader from './assets/dart-wader.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo}  alt="logo" />
       </header>
+      <footer>
+      <img src={dartWader}  alt="logo" />
+      </footer>
     </div>
   );
 }
